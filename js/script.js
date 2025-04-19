850e4625e3ab4192f2edc0f79f1d306036f2a3a3
Dimi <johnnnathan@proton.me>
2025-04-19 12:44:16 +0200

    Cool?

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ window.onload = function () {
 
             const challengeList = document.getElementById("challenge-list");
 
-            
             if (Array.isArray(data.challenges) && data.challenges.length > 0) {
                 data.challenges.forEach(challenge => {
-                    const listItem = document.createElement("li");
+                    const challengeFrame = document.createElement("div");
+                    challengeFrame.classList.add("challenge-frame");
+
                     const link = document.createElement("a");
-                    
-                    // Update link to include the correct path
                     link.href = `challenges/${challenge}/index.html`; // Correct path
                     link.textContent = challenge.replace(/_/g, ' ');  // Replace underscores with spaces
-                    listItem.appendChild(link);
-                    challengeList.appendChild(listItem);
+
+                    challengeFrame.appendChild(link);
+                    challengeList.appendChild(challengeFrame);
                 });
             } else {
                 console.log("No challenges found or invalid format.");
